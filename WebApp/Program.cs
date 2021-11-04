a3998c9194a359f55056b51bd3d1b3eccde4201c
eldar258 <9eldik7@gmail.com>
2021-11-04 10:56:59 +0300

    Getting rid of deprecated functionality

--- WebApp/Program.cs
+++ WebApp/Program.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Hosting;
 
 namespace WebApp
 {
@@ -10,9 +11,12 @@ namespace WebApp
             CreateWebHostBuilder(args).Build().Run();
         }
 
-        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
-                WebHost.CreateDefaultBuilder(args)
-                       .UseStartup<Startup>();
+        public static IHostBuilder CreateWebHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+            .ConfigureWebHostDefaults(webBuilder =>
+            {
+                webBuilder.UseStartup<Startup>();
+            });
 
         /*
          * TODO 6: Fix issue
